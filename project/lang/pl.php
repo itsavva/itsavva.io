e0df61aac5566de42c86a9f7d9a2ec1f19397d6b
itsavva <savvamaksym@gmail.com>
2025-08-24 20:20:29 +0300

    pl.php

---
+++ project/lang/pl.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    "services" => "Usługi",
+    "about"    => "O nas",
+    "contact"  => "Kontakt",
+    "hero_h1"  => "Profesjonalny serwis samochodowy",
+    "hero_p"   => "Chip tuning, układy wydechowe, diagnostyka.",
+    "services_title" => "NASZE USŁUGI",
+    "card1_title" => "Chip tuning Stage-1",
+    "card1_text"  => "Bezpieczny chip tuning Stage 1 zwiększający moc i moment obrotowy bez zmiany fabrycznych części.",
+];
